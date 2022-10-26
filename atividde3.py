16a48335c1b5c70d9ee282de21756dbf8f14342d
diogomagalhaesilva2.0@gmail.com <888703@senacminas.edu.br>
2022-10-26 20:16:36 -0300

    atividad3

---
+++ atividde3.py
@@ -0,0 +1,14 @@
+# Crie um programa que tenha a, b, c.
+print ("Programa para calculos matematicos")
+a = float(input("Digite um numero inteiro: "))
+b = float(input("Digote outro numero inteiro: "))
+resultado = a + b
+print ("A soma dos dois numeros é: ", resultado)
+resultado = a - b
+print ("A subtração dos numeros é: ", resultado)
+resultado = a * b
+print ("A soma da multiplicação dos numeros é: ", resultado)
+resultado = a / b
+print ("A divisão dos numeros é: ", resultado)
+resultado = a ** b
+print ("A elevação dos numeros é: ", resultado)
