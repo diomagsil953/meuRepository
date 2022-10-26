27954114d4135b4fde9092fb42160cd0c2cd955a
diogomagalhaesilva2.0@gmail.com <888703@senacminas.edu.br>
2022-10-25 21:51:58 -0300

    atividade1

---
+++ atividade1.py
@@ -0,0 +1,9 @@
+# Crie um programa que contenha as variaveis a, b, c, d.
+a = 12
+b = 6.8
+c = "vaca preta"
+d = True
+print (a)
+print (b)
+print (c)
+print (d)
---
+++ atividade2.py
@@ -0,0 +1,4 @@
+# Crie um programa que contenha as variaveis a, b.
+a = 29
+b = "Vasco"
+print ("O valor da variavel a é: " + str(a))
