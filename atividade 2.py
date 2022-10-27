e951f07ee3a08323265d672e3505528b3f406a2d
diogomagalhaesilva2.0@gmail.com <888703@senacminas.edu.br>
2022-10-26 21:54:20 -0300

    atividade2

---
+++ atividade 2.py
@@ -0,0 +1,20 @@
+# Crie um programa com 5 variaveis constantes
+iPhone = float(2.980)
+samsung = float(2.540)
+tablet = float(1.950)
+ps5 = float(2.100)
+notebook = float(2.350)
+ quantidade = float(input(“quantos iphone você quer compra?))
+ quantidade 2 = float(“quantos samsung você quer compra?))
+ quantidade 3 = float(“quantos tablet quer compra?))
+ quantidade 4 = float(“quantos PS5 você quer compra?”))
+ quantidade 5 = float(“quantos notebook você quer compra?”))
+ valor 1 = float(
+ valor 2 = float(
+ valor 3 = float(
+ numero =float(
+print (input(“quantos iphone você quer compra?” ))
+print (input(“quantos samsung você quer compra?” quantidade 2))
+print (input(“quantos tablet você quer compra?” quantidade 3))
+print (input(“quantos PS5 você quer compra?” quantidade 4))
+print (input(“quantos notebook você quer compra?” quantidade 5))
